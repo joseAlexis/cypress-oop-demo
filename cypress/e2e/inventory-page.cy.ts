e252214c254221543964b2469aa327b79005449b
Jose Bolanos <joseAlexis>
2024-05-23 22:56:06 +0200

    Minor updates in tests

--- cypress/e2e/inventory-page.cy.ts
+++ cypress/e2e/inventory-page.cy.ts
@@ -9,18 +9,21 @@ describe("Inventory page", () => {
 
   it("Should add an item to the cart", () => {
     const item = "Sauce Labs Bolt T-Shirt";
+
     inventoryPage.addToCart(item);
     inventoryPage.getCartBadge().should("have.text", 1);
   });
 
   it("Should open a product", () => {
     const itemName = "Sauce Labs Bike Light";
+
     inventoryPage.navigateToItem(itemName);
     itemPage.elements.name().should("be.visible").and("have.text", itemName);
   });
 
   it("Should navigate to your cart", () => {
     const itemName = "Sauce Labs Onesie";
+
     inventoryPage.addToCart(itemName);
     inventoryPage.navigateToCart();
     cartPage.getItemQuantity(0).should("be.visible").and("have.text", "1");
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,19 +1,11 @@
 import "cypress-localstorage-commands";
 
-import { cartPage } from "./pages/CartPage";
-import Item from "./model/Item";
 import { loginPage } from "./pages/LoginPage";
 
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Cypress {
     interface Chainable {
-      /**
-       * Validate the items previously stored in the cart
-       * @param Item Object with the item information
-       */
-      validateCartItems(Item: Item): void;
-
       /**
        * User login
        * @param username
@@ -24,16 +16,6 @@ declare global {
   }
 }
 
-Cypress.Commands.add("validateCartItems", (itemDTO: Item) => {
-  cartPage.elements.listCartItems().each((item) => {
-    expect(item.find("div.inventory_item_name").text()).to.eq(itemDTO.name);
-    expect(item.find("div.inventory_item_desc").text()).to.eq(
-      itemDTO.description
-    );
-    expect(item.find("div.inventory_item_price").text()).to.eq(itemDTO.price);
-  });
-});
-
 Cypress.Commands.add("appLogin", (username: string, password: string) => {
   loginPage.visit();
   loginPage.elements.loginForm().should("be.visible");
@@ -42,9 +24,5 @@ Cypress.Commands.add("appLogin", (username: string, password: string) => {
   loginPage.typePassword(password);
   loginPage.clickLogin();
 
-  cy.getCookies()
-    .should("have.length", 1)
-    .then((cookies) => {
-      expect(cookies[0].value).to.eq(username);
-    });
+  cy.document().its("cookie").should("contain", "session-username");
 });
